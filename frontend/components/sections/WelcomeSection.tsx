775267f8f2f24b4d0669cb4d06f55b0995d8cca3
chay1504 <chaitanyaseema1504@gmail.com>
2026-05-27 12:10:42 +0530

    feat(welcome): add College Circle logo to partners scrolling marquee

--- frontend/components/sections/WelcomeSection.tsx
+++ frontend/components/sections/WelcomeSection.tsx
@@ -16,6 +16,7 @@ const messages = [
 export function WelcomeSection() {
   const logos = [
     "/rtih_logo.png",
+    "https://www.collegecircle.cc/assets/cclogo.PNG",
     "https://dc-cdn.s3-ap-southeast-1.amazonaws.com/dc-Cover-4upece6p9dvb9msn1tlcgsivc6-20160519052405.Medi.jpeg",
     "https://incuxai.com/assets/img/logo/incuxai.jpg",
     "https://upload.wikimedia.org/wikipedia/en/e/ea/Appolice%28emblem%29.png",
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -22,6 +22,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'www.incuxai.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.collegecircle.cc',
+      },
     ],
   },
 };
